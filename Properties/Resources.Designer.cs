5222dd544d34af296135d5601be21d377ad49ac9
neriala <neria616@gmail.com>
2023-05-22 18:54:09 +0300

    added gifs

--- Properties/Resources.Designer.cs
+++ Properties/Resources.Designer.cs
@@ -103,6 +103,16 @@ namespace BirdManagment.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap cage_unscreen {
+            get {
+                object obj = ResourceManager.GetObject("cage-unscreen", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap home {
             get {
                 object obj = ResourceManager.GetObject("home", resourceCulture);
