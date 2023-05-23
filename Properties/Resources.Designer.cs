ae68b5e800071495ecda13d66e617964f17f4d60
neriala <neria616@gmail.com>
2023-05-23 11:22:34 +0300

    unscreen bird cage

--- Properties/Resources.Designer.cs
+++ Properties/Resources.Designer.cs
@@ -93,16 +93,6 @@ namespace BirdManagment.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
-        internal static System.Drawing.Bitmap cage {
-            get {
-                object obj = ResourceManager.GetObject("cage", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized resource of type System.Drawing.Bitmap.
-        /// </summary>
         internal static System.Drawing.Bitmap cage_unscreen {
             get {
                 object obj = ResourceManager.GetObject("cage-unscreen", resourceCulture);
